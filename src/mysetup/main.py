f378feef22346e8e967e62378546759da6c58ca3
Job Cabral <mr.bojboj@icloud.com>
2023-02-09 23:43:36 -0800

    Completed basic set up.

--- src/mysetup/main.py
+++ src/mysetup/main.py
@@ -1,9 +1,20 @@
+import os
+from pathlib import Path
+
 import typer
 
 
-def main(name: str):
-    print(f"Hello {name}")
+def main():
+    config = Path.home().joinpath('.config')
+    myconfig = config.joinpath('myconfig')
+    nvim = config.joinpath('nvim')
 
+    print(f'updating {myconfig}')
+    os.system(f'cd {myconfig} && git pull')
 
+    print(f'updating {nvim}')
+    os.system(f'cd {nvim} && git pull')
+    os.system(f'cd {nvim} && nvim -S plugsnapshot.vim')
+    
 if __name__ == "__main__":
     typer.run(main)
