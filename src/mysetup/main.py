bcb4794a8102a0440282f24f5266b7b0496bf23b
Job Cabral <mr.bojboj@icloud.com>
2023-02-10 01:29:55 -0800

    Updated script.

--- src/mysetup/main.py
+++ src/mysetup/main.py
@@ -5,20 +5,25 @@ import typer
 
 
 def main():
-    config = Path.home().joinpath('.config')
-    myconfig = config.joinpath('myconfig')
-    projectconfig = config.joinpath('projectconfig')
-    nvim = config.joinpath('nvim')
+    config = Path.home().joinpath(".config")
+    myconfig = config.joinpath("myconfig")
+    projectconfig = config.joinpath("projectconfig")
+    nvim = config.joinpath("nvim")
 
-    print(f'updating {myconfig}')
-    os.system(f'cd {myconfig} && git pull')
+    print(f"updating {myconfig}")
+    os.system(f"cd {myconfig} && git pull")
 
-    print(f'updating {projectconfig}')
-    os.system(f'cd {projectconfig} && git pull')
+    print(f"updating {projectconfig}")
+    os.system(f"cd {projectconfig} && git pull")
 
-    print(f'updating {nvim}')
-    os.system(f'cd {nvim} && git pull')
-    os.system(f'cd {nvim} && nvim -S plugsnapshot.vim')
-    
+    print(f"updating {nvim}")
+    os.system(f"cd {nvim} && git pull")
+
+    print("restoring neovim plugins from snapshot")
+    os.system(f"cd {nvim} && nvim -S plugsnapshot.vim +qa")
+
+    print("done")
+
+
 if __name__ == "__main__":
     typer.run(main)
